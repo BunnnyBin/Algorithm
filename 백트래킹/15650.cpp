b78cc01bf091aff61c38728ee1127118cf39c74b
BunnyBin <dbqls8428@gmail.com>
2020-05-15 14:20:18 +0900

    백트래킹 기초 - N과M
    5/17

---
+++ 백트래킹/15650.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define MAX 8
+
+using namespace std;
+
+int arr[MAX];
+bool visited[MAX];
+int cnt, idx, n, m;
+
+void Print() {
+	for (int i = 0; i < n; i++)
+		if (visited[i]) {
+			cout << arr[i] << " ";
+		}
+	cout << '\n';
+}
+
+void DFS(int cnt, int idx) {
+	if (cnt == m) {
+		Print();
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (visited[i]) continue;
+		visited[i] = true;
+
+		DFS(cnt + 1, i + 1);
+		visited[i] = false;
+	}
+}
+
+int main(void) {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+		visited[i] = false;
+	}
+
+	DFS(0, 0);
+}
