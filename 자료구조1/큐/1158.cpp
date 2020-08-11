234dc989602cdfc7b8a63a7e78695c72b5129621
BunnyBin <dbqls8428@gmail.com>
2020-08-12 01:40:26 +0900

    요세푸스 문제

---
+++ 자료구조1/큐/1158.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue <int> q;
+
+int main(void) {
+	int n, k;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	cout << "<";
+	while (!q.empty()) {
+		for (int i = 0; i < k - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty())
+			cout << ',' << ' ';
+	}
+	cout << '>';
+}
